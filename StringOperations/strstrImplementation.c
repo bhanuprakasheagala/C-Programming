066c181e3ae8135d2a6cf43ff095adfa88850c38
bhanuprakasheagala <bhanuprakasheagala@gmail.com>
2024-12-05 20:47:40 +0530

    Update the code to support runtime user input receiver function

--- StringOperations/strstrImplementation.c
+++ StringOperations/strstrImplementation.c
@@ -2,6 +2,40 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Function to read the input from user
+char* readString() {
+    int bufferSize = 10;
+    int length = 0;
+    char* actualBuffer = malloc(bufferSize * sizeof(char));   // Allocate initial memory
+
+    if(actualBuffer == NULL) {
+        printf("Memory allocation failed!!\n");
+        return NULL;
+    }
+
+    int ch;
+    while((ch = getchar()) != '\n' && (ch != EOF)) {
+        if(length >= (bufferSize - 1)) {
+            bufferSize *= 2;        // Double the size of buffer
+            char* newBuffer = realloc(actualBuffer, bufferSize * sizeof(char));
+
+            if(newBuffer == NULL) {
+                free(actualBuffer);
+                printf("Memory allocation failed!\n");
+                return NULL;
+            }
+
+            actualBuffer = newBuffer;
+        }
+
+        actualBuffer[length++] = ch;
+    }
+
+    actualBuffer[length] = '\0';        // Null-terminate the string
+    
+    return actualBuffer;
+}
+
 char* my_strstr(const char* haystack, const char* needle) {
     // If needle is an empty string, return the haystack
     if(*needle == '\0') {
@@ -31,8 +65,12 @@ char* my_strstr(const char* haystack, const char* needle) {
 }
 
 int main() {
-    char* str = "Hello, World!";
-    char* sub = "World";
+    
+    printf("Enter the main string: \n");
+    char* str = readString();
+    
+    printf("Enter the substring that needs to be searched in the main string: \n");
+    char* sub = readString();
 
     char* result = my_strstr(str, sub);
 
